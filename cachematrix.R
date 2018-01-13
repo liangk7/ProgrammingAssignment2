d0802b4601e77f62ce38c2728bb317434ec7a1a5
liangk7 <kyle.liang.7@gmail.com>
2018-01-12 16:59:50 -0800

    added explanatory comments to functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
+## [Put comments here that give an overall description of what your
+## functions do]
 
-## Write a short comment describing this function
-
+## [Write a short comment describing this function]
+## The following function creates a special matrix that can retain
+## its inverse in cache
+## Variables
+## 'x' as the matrix, 'xinv' as the matrix inverse
+## Functions
+## 'set', 'get' to set and get value of x
+## 'setinv', 'getinv' to set and get value of xinv
 makeCacheMatrix <- function(x = matrix()) {
     xinv <- NULL
     set <- function(y) {
@@ -10,16 +16,19 @@ makeCacheMatrix <- function(x = matrix()) {
     	xinv <<- NULL
     }
     get <- function() x
-    setinv <- function(solve) xinv <<- solve
+    setinv <- function(inv) xinv <<- inv
     getinv <- function() xinv
     list(set = set, get = get,
      	setinv = setinv,
      	getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## [Write a short comment describing this function]
+## The following function allows one to pull the inverse of a
+## matrix (whether it already exists in cache or not)
+## Variables
+## 'x' as the matrix object, 'xinv' as the matrix inverse
+## 'data' as the matrix
 cacheinv <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     xinv <- x$getinv()
